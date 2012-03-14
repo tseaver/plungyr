ab73ca5ef5ba5f5fe71b6eeb20c870ab63f36329
Tres Seaver <tseaver@palladion.com>
2012-03-13 21:17:51 -0400

    Begin reshaping homepage view.

--- plungyr/tests/test_views.py
+++ plungyr/tests/test_views.py
@@ -3,14 +3,20 @@ import unittest
 from pyramid import testing
 
 class ViewTests(unittest.TestCase):
+
     def setUp(self):
         self.config = testing.setUp()
 
     def tearDown(self):
         testing.tearDown()
 
+    def _callFUT(self, context, request):
+        from ..views import plungyr_main
+        return plungyr_main(context, request)
+        
+
     def test_my_view(self):
-        from ..views import my_view
+        context = testing.DummyModel()
         request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'plungyr')
+        info = self._callFUT(context, request)
+        self.assertEqual(info['purl'], 'http://example.com/')
--- plungyr/views.py
+++ plungyr/views.py
@@ -1,6 +1,8 @@
 from pyramid.view import view_config
+from pyramid.url import resource_url
+
 from .models import Plungyr
 
 @view_config(context=Plungyr, renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project':'plungyr'}
+def plungyr_main(context, request):
+    return {'purl': resource_url(context, request)}
